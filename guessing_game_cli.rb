86e4763b734cf0a13995100e5ad053a84daa9997
Jacob Kenny <jacobkenny05@gmail.com>
2019-12-06 23:59:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,16 @@
 # Code your solution here!
+require 'pry'
+ #generate and store a random number between 1 and 6
+ #prompt the user to guess their own number between 1 and 6
+ #capture user input from the command line 
+ #Compare that input to the random number that has been generated
+ #print out one of three statements:
+      #"You guessed the correct number!"
+      #"Sorry! The computer guessed <number>."
+      #if user input is equal to "exit": "Goodbye!"
+
+def run_guessing_game(num)
+random_number = rand(1..6)
+user_input = 
+binding.pry 
+end   
